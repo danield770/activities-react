88c88fc7478d4165143db04b6150fcf51f9e0866
danield770 <dddavid2@gmail.com>
2023-07-04 16:23:30 +0300

    add vercel.json again

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  appType: 'mpa', // disable history fallback
   server: {
     port: 8080,
     proxy: {
@@ -13,6 +14,11 @@ export default defineConfig({
         secure: false,
         // rewrite: (path) => path.replace(/^\/#/, ''),
       },
+      // '^/activities/.*': {
+      //   target: 'http://localhost:3000',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/activities/, ''),
+      // },
     },
   },
 });
